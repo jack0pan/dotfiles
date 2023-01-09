6e3bebf587246573ff80cedcadc34d8d11ac6dcb
Pan Jie <fenqi@startdt.com>
2023-01-09 20:05:13 +0800

    feat: update fish config

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -12,6 +12,8 @@ alias python=python3
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
 
+set -gx LANG en_US.UTF-8
+set -gx LC_ALL en_US.UTF-8
 set -gx GOPATH ~/Projects/Go
-set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/openjdk.jdk/Contents/Home
-set -gx PATH $GOPATH/bin:$JAVA_HOME/bin:$PATH
+
+set -U fish_user_paths $fish_user_paths $GOPATH/bin
