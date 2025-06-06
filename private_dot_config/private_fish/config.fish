58291f763f43d6e64b9fe28bd11ff5bcfc01350e
Pan Jie <fenqi@startdt.com>
2025-06-06 11:40:43 +0800

    feat: add zed config

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -4,13 +4,7 @@ end
 
 starship init fish | source
 
-rvm default
-
 alias python=python3
-
-# tabtab source for packages
-# uninstall by removing these lines
-[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
 
 set -gx LANG en_US.UTF-8
 set -gx LC_ALL en_US.UTF-8
