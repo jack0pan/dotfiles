c821841d08f5b7dcec92199596be5d5fc0103d19
Jack Pan <i@jackpan.me>
2025-06-17 22:47:01 +0800

    feat: update fish config

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,12 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-starship init fish | source
+set -gx LANG en_US.UTF-8
+set -gx LC_ALL en_US.UTF-8
 
 alias python=python3
 
-set -gx LANG en_US.UTF-8
-set -gx LC_ALL en_US.UTF-8
-set -gx GOPATH ~/Projects/Go
+starship init fish | source
 
-set -U fish_user_paths $fish_user_paths $GOPATH/bin
