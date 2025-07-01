46ad2f48acfbbb8ce9bf03a7dabb044e9ee1cc66
Jack Pan <i@jackpan.me>
2025-07-01 22:45:44 +0800

    chore: resolve conflicts

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,12 +2,18 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -gx LANG en_US.UTF-8
-set -gx LC_ALL en_US.UTF-8
+alias python=python3
 
 fish_config theme choose "Dracula Official"
 
-alias python=python3
+set -gx LANG en_US.UTF-8
+set -gx LC_ALL en_US.UTF-8
+if test -d (brew --prefix)"/share/fish/completions"
+    set -p fish_complete_path (brew --prefix)/share/fish/completions
+end
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
 
 starship init fish | source
 
