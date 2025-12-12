141e77b3b92ce9627a8fa1bbf2cc6b1d268f49df
Pan Jie <fenqi@startdt.com>
2025-12-12 15:03:55 +0800

    feat: setup go and rust

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -10,6 +10,7 @@ set -gx LANG en_US.UTF-8
 set -gx LC_ALL en_US.UTF-8
 set -gx TERM xterm-256color
 set -gx EDITOR vim
+set -gx GOPATH $HOME/Projects/Go
 
 if test -d (brew --prefix)"/share/fish/completions"
     set -p fish_complete_path (brew --prefix)/share/fish/completions
@@ -23,4 +24,5 @@ starship init fish | source
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
 
 fish_add_path "$HOME/.bun/bin"
+fish_add_path /opt/homebrew/opt/rustup/bin
 
