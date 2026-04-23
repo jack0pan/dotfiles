8c1e05ef7d04f7892713a7cfda5b345ca3a3bcb0
Pan Jie <fenqi@startdt.com>
2026-04-23 16:29:40 +0800

    fix: zed and fish config

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -25,10 +25,6 @@ set -l output (mole completion fish 2>/dev/null); and echo "$output" | source
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
 
 fish_add_path "$HOME/.bun/bin"
-fish_add_path /opt/homebrew/opt/rustup/bin
-<<<<<<< Updated upstream
-fish_add_path "$HOME/.antigravity/antigravity/bin"
 fish_add_path "$HOME/.local/bin"
-=======
->>>>>>> Stashed changes
+fish_add_path /opt/homebrew/opt/rustup/bin
 
